7ba628043e4e55b9fb0e6a63d84690e9df30faee
Favour <jammoben@gmail.com>
2024-05-02 20:25:16 +0100

    handled an error with small input files

--- server/main.py
+++ server/main.py
@@ -5,22 +5,25 @@ from glob import glob
 
 def breakStartAndEndChuck(audioFile, timeLength: int):
     """
+    Breaks an audio file into three chunks: start, middle and end.
+    Input: audioFile: path to audio file
+            timeLength: length of each chunk in sec
+    Output: startChunk, midChunk, endChunk
     """
+    # load audio and sample rate into memory
     audio, sampleRate = lr.load(audioFile)
-
+    # calculate number of samples in each chunk based on the timeLength
     chunkSamples = int(sampleRate * timeLength)
-
+    # calculate total number of chunks
     totalChunks = len(audio) // chunkSamples
+    # print(f"Total chunks: {totalChunks}")
+    # split the audio file into chunks using python's slice operator
 
-    print(f"Total chunks: {totalChunks}")
-
-
+    if totalChunks == 0:
+        raise Exception("Audio file is too short.")
     startChunk = audio[:chunkSamples]
-    print(len(startChunk))
     midChunk = audio[chunkSamples:-chunkSamples]
-    print(len(midChunk))
     endChunk = audio[-chunkSamples:]
-    print(len(endChunk))
 
     return startChunk, midChunk, endChunk
 
@@ -28,5 +31,8 @@ def breakStartAndEndChuck(audioFile, timeLength: int):
 if __name__ == "__main__":
     timeStamp = 10 #in millisec (10 seconds)
     audioFile = "test.mp3"
-    startChunk, midChunk, endChunk = breakStartAndEndChuck(audioFile, timeStamp)
-    # print("Start chunk: ", startChunk, "\nMid chunk: ", midChunk, "\nEnd chunk: ", endChunk)
+    try:
+        startChunk, midChunk, endChunk = breakStartAndEndChuck(audioFile, timeStamp)
+    except Exception as e:
+        if repr(e) == "Audio file is too short.":
+            print("Audio file is too short.")
